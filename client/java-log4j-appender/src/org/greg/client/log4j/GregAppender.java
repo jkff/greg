32caab9d4f5e7095537f4db2fc716497444f3fec
Eugene Kirpichov <ekirpichov@griddynamics.com>
2010-12-03 16:49:44 +0300

    Added log4j appender

---
+++ client/java-log4j-appender/src/org/greg/client/log4j/GregAppender.java
@@ -0,0 +1,22 @@
+package org.greg.client.log4j;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+import org.greg.client.Greg;
+
+public class GregAppender extends AppenderSkeleton {
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
+        Layout layout = getLayout();
+        Greg.log(layout == null ? loggingEvent.getRenderedMessage() : getLayout().format(loggingEvent));
+    }
+
+    public void close() {
+        // Nothing
+    }
+
+    public boolean requiresLayout() {
+        return false;
+    }
+}
